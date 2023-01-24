5e5a316518a0d78ebf197cd876987e74bb109ad2
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-01-24 12:49:16 +0700

    some setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import '@picocss/pico'
+import '@/styles/globals.css'
+import Link from 'next/link'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <nav>
+        <ul>
+          <li><Link href="/">codegames</Link></li>
+        </ul>
+      </nav>
+      <Component {...pageProps} />
+    </>
+  )
 }
